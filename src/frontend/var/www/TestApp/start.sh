aadbc3272ebbbe7a4ec8c3b691301c1e38fd3c52
gregn <greg.nicol@nominet.uk>
2020-10-30 10:21:18 +0000

    change frontend port

--- src/frontend/var/www/TestApp/start.sh
+++ src/frontend/var/www/TestApp/start.sh
@@ -10,6 +10,6 @@ set -o pipefail
 
 set -x
 
-app="docker.test"
+app="demo.frontend"
 docker build -t ${app} .
-docker run -d -p 56733:80 --name=${app} -v $PWD:/app ${app}
+docker run -d -p 58080:80 --name=${app} -v $PWD:/app ${app}
