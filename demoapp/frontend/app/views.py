69f1fc11fef23a9802b5c11e2191025d77ee5719
gregn <greg.nicol@nominet.uk>
2020-10-30 16:41:38 +0000

    de-hardcode backend API URL

--- demoapp/frontend/app/views.py
+++ demoapp/frontend/app/views.py
@@ -1,6 +1,12 @@
 from flask import render_template
 from app import app
+from os import getenv
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    ctx = {"BACKEND_API_PROTOCOL": getenv('BACKEND_API_PROTOCOL', 'http'),
+           "BACKEND_SERVICE_HOST": getenv('BACKEND_SERVICE_HOST', 'localhost'),
+           "BACKEND_SERVICE_PORT": getenv('BACKEND_SERVICE_PORT', '58081'),
+           "BACKEND_API_PATH_COLOUR": getenv('BACKEND_API_PATH_COLOUR', 'api/v1/colour.json'),
+           }
+    return render_template('index.html', **ctx)
