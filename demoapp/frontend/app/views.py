2e3fc7c673e6f6ad4585feb74c0010de08169caf
gregn <greg.nicol@nominet.uk>
2020-10-30 17:55:45 +0000

    want serverside frontend -> backend, not SPA

--- demoapp/frontend/app/views.py
+++ demoapp/frontend/app/views.py
@@ -1,12 +1,20 @@
 from flask import render_template
 from app import app
 from os import getenv
+from urllib import request
+
+import logging
 
 @app.route('/')
 def home():
-    ctx = {"BACKEND_API_PROTOCOL": getenv('BACKEND_API_PROTOCOL', 'http'),
-           "BACKEND_SERVICE_HOST": getenv('BACKEND_SERVICE_HOST', 'localhost'),
-           "BACKEND_SERVICE_PORT": getenv('BACKEND_SERVICE_PORT', '58081'),
-           "BACKEND_API_PATH_COLOUR": getenv('BACKEND_API_PATH_COLOUR', 'api/v1/colour.json'),
-           }
+    url = "{}://{}:{}/{}".format(
+        getenv('BACKEND_API_PROTOCOL', 'http'),
+        getenv('BACKEND_SERVICE_HOST', 'localhost'),
+        getenv('BACKEND_SERVICE_PORT', '58081'),
+        getenv('BACKEND_API_PATH_COLOUR', 'api/v1/colour.json'),
+    )
+    logging.warning("API URL: %s", url)
+    # ToDo:
+    data = request.urlopen(url).read().decode('utf-8', 'replace')
+    ctx = {"JSON_DATA": data}
     return render_template('index.html', **ctx)
