c42e92be264f6959f6340c1582ed3026896dcdf4
gregn <greg.nicol@nominet.uk>
2020-10-30 10:39:44 +0000

    Change backend response mime type

--- src/backend/var/www/TestApp/app/views.py
+++ src/backend/var/www/TestApp/app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, Response
 from app import app
 
 @app.route('/')
@@ -7,4 +7,9 @@ def home():
 
 @app.route('/api/v1/colour.json')
 def template():
-    return render_template('v1/colour.json')
+    ret = render_template('v1/colour.json')
+    resp = Response(response=ret,
+                    status=200,
+                    mimetype="application/json")
+
+    return resp
